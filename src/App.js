9631842497f63dfcdb8acb5f2109ad8187486bde
Joe Saraceno <josephasaraceno@gmail.com>
2019-10-11 23:16:42 -0600

    tidy up headings area

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+// libs
 import React, { Component, Fragment } from 'react';
-import { getTemperatureFeeds } from './utils/TemperatureUtils';
 import styled from 'styled-components';
-import { Button } from 'semantic-ui-react';
+import { Button, Header } from 'semantic-ui-react';
+import { format } from 'date-fns';
 
+// utils
+import { getTemperatureFeeds } from './utils/TemperatureUtils';
+
+// components
 import Feeds from './components/Feeds/Feeds';
 
+// styled components
 const PageContainer = styled.div`
   height: 100%;
   width: 100%;
   margin-top: 3em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const FeedWrapper = styled.div`
-  margin-top: 3em;
+  margin-top: 2em;
 `;
 
 const ButtonWrapper = styled.div`
@@ -22,6 +31,19 @@ const ButtonWrapper = styled.div`
   margin-bottom: 3em;
 `;
 
+const SubHeading = styled.p`
+  text-align: center;
+  font-size: 12px;
+  color: grey;
+  opacity: .6;
+  margin-bottom: 0;
+  padding-bottom: 0;
+`;
+
+const HeadingWrapper = styled.div`
+  max-width: 300px;
+`;
+
 class App extends Component {
 
   constructor(props) {
@@ -29,7 +51,8 @@ class App extends Component {
     this.state = {
       loading: true,
       results: null,
-      errMessage: null
+      errMessage: null,
+      date: null,
     }
   }
   render() {
@@ -44,14 +67,18 @@ class App extends Component {
       const { channel, feeds } = results;
       const { name, description } = channel;
       const channelId = channel.id;
+      const latestDate = this.getDateOfLatestResult(feeds[0]);
       return (
         <PageContainer>
-          <h2 className="ui center aligned header">{ name }</h2>
-          <h3 className="ui center aligned header">{description }</h3>
-          <h4 className="ui center aligned header">ID# { channelId }</h4>
+          <HeadingWrapper>
+            <Header as='h2' textAlign='center'>{ name }</Header>
+            <Header as='h4' textAlign='center'>{ description }</Header>
+            <Header as='h4' textAlign='center'> { latestDate }</Header>
+            <SubHeading >Channel #{ channelId }</SubHeading>
+          </HeadingWrapper>
           <FeedWrapper className="ui container">
             <ButtonWrapper>
-              <Button onClick={(e) => {this.getResults(e)}} color="teal">Newest Readings</Button>
+              <Button color="teal" onClick={() => {this.getResults()}}>Get New Readings</Button>
             </ButtonWrapper>
             <Feeds feeds={feeds} loading={loading}></Feeds>
           </FeedWrapper>
@@ -61,10 +88,7 @@ class App extends Component {
     return <Fragment></Fragment>
   }
 
-  async componentDidMount() {
-    this.getResults();
-  }
-
+  // fetch results from util
   async getResults() {
     this.setState({ loading: true })
     try {
@@ -74,6 +98,17 @@ class App extends Component {
       this.setState({ errMessage: err.message, loading: false });
     }
   }
+  
+  // perform the initial fetch
+  async componentDidMount() {
+    this.getResults();
+  }
+
+  getDateOfLatestResult(feed) {
+    const { created_at } = feed;
+    return format(new Date(created_at), 'eeee, MM/dd/yyyy');
+  }
+
 };
 
 export default App;
