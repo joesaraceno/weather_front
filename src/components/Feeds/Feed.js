3870edece93b3279a6e0e9513f4a8d8ba630718d
Joe Saraceno <josephasaraceno@gmail.com>
2019-10-12 00:18:25 -0600

    add trend line values

--- src/components/Feeds/Feed.js
+++ src/components/Feeds/Feed.js
@@ -3,7 +3,11 @@ import styled from 'styled-components'
 import { format } from 'date-fns';
 import { Card } from 'semantic-ui-react';
 
-const Field = styled.p`
+const Time = styled.p`
+  font-size: 18px;
+`;
+
+const Temp = styled.p`
   font-size: 22px;
 `;
 
@@ -29,19 +33,19 @@ const determineColor = (temp) => {
 };
 
 export default function Feed ({ feed }) {
-  const { created_at, entry_id, field1 } = feed;
-  const formattedSampleDate = format (new Date (created_at), 'h:mm:ss aaaa');
+  const { created_at, entry_id, field1, trend } = feed;
+  const formattedSampleDate = format (new Date (created_at), 'h:mm:ss aa');
   const color = determineColor(field1);
   return ( 
     <Card color={color}>
-      <Card.Content textAlign={'right'}>
-        <Card.Header>Temperature Reading</Card.Header>
-        <Card.Meta>Entry# {entry_id}</Card.Meta>
+      <Card.Content textAlign='right'>
         <Card.Description>
-          <Field><b>{ field1 }</b> &deg;F</Field>
-          <Field>{ formattedSampleDate }</Field>
+          <Time>{ formattedSampleDate }</Time>
+          <Temp><b>{ field1 }</b> &deg;F</Temp>
         </Card.Description>
+        <Card.Meta>Entry# {entry_id}</Card.Meta>
       </Card.Content>
+      <Card.Content extra textAlign='center'>{ trend }</Card.Content>
     </Card>
   )
 }
--- src/components/Feeds/Feeds.js
+++ src/components/Feeds/Feeds.js
@@ -2,6 +2,28 @@ import React from 'react';
 import { Card, Dimmer, Grid, Image, Loader, Segment } from 'semantic-ui-react';
 
 import Feed from './Feed';
+import loaderImage from './short-paragraph.png';
+
+const sortFeeds = function(a,b) {
+  return a.entry_id < b.entry_id ? 1 : -1;
+};
+
+// add a trend value to each feed item
+const addMetaToFeeds = (feeds) => {
+  feeds.forEach((feed, index) => {
+    const thisTemp = feed.field1;
+    if (index < 1) {
+      return feed.trend = 0;
+    }
+    const lastTemp = feeds[index-1].field1;
+    if (lastTemp < thisTemp) {
+      return feed.trend = 1;
+    } else if (lastTemp > thisTemp) {
+      return feed.trend = -1;
+    }
+    return feed.trend = 0;
+  })
+}
 
 export default function Feeds ({ feeds, loading }) {
   if (loading) {
@@ -10,23 +32,20 @@ export default function Feeds ({ feeds, loading }) {
         <Dimmer active inverted>
           <Loader inverted content='Loading' />
         </Dimmer>
-        <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+        <Image src={ loaderImage } alt="Loading" />
       </Segment>
     );
   }
-
-  var sortFeeds = function(a,b) {
-    return a.entry_id < b.entry_id ? 1 : -1;
-  };
-
+  
+  addMetaToFeeds(feeds);
   const sortedFeeds = feeds.sort(sortFeeds);
 
   return (
     <Grid columns={1} divided>
       <Card.Group itemsPerRow={5} doubling>
-        {sortedFeeds.map(feed => {
-          return <Feed key={feed.entry_id} feed={feed} />
-        })}
+        { sortedFeeds.map(feed => {
+          return <Feed key={ feed.entry_id } feed={ feed } />
+        }) }
       </Card.Group>
     </Grid>
   )
